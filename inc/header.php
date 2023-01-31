ba56366fa5df0e4eae815995df6badb2d6ffb9eb
LudwigELATRE <73239553+LudwigELATRE@users.noreply.github.com>
2023-01-31 19:41:34 +0100

    commit 19:41

--- inc/header.php
+++ inc/header.php
@@ -5,30 +5,35 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Psychologue</title>
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <title>Maeve Divad</title>
+    <link rel="shortcut icon" href="assets/icon/favicon.png" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" href="reset.css">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://kit.fontawesome.com/351f12c0da.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 
 <body>
     <header>
-        <div>
-            <img src="" alt="logo">
-        </div>
-        <div>
-            <div>
-                <h1>Psychologue clinicienne</h1>
-                <h4>Spécialisée en gérontologie, psychothérapie adultes et personnes âgées</h4>
+        <div class="container-header">
+            <div class="container-header-left">
+                <img src="assets/images/logo.png" class="logo animate__animated animate__fadeInDown" alt="logo">
             </div>
-            <div>
-                <ul>
-                    <li><a href="#contact">A propos de moi</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                    <li><a href="#contact">Therapie</a></li>
-                    <li><a href="#contact">Tarifs</a></li>
-                </ul>
+            <div class="container-header-right">
+                <div class="header-title">
+                    <h1>Psychologue clinicienne</h1>
+                    <h4>Spécialisée en gérontologie, psychothérapie adultes et personnes âgées</h4>
+                </div>
+                <div class="nav-bar-header">
+                    <ul>
+                        <li><a href="#contact">Acceuil</a></li>
+                        <li><a href="#contact">A propos</a></li>
+                        <li><a href="#contact">Therapie</a></li>
+                        <li><a href="#contact">Publications</a></li>
+                        <li><a href="#contact">Tarifs</a></li>
+                    </ul>
+                </div>
             </div>
         </div>
     </header>
--- index.php
+++ index.php
@@ -1,17 +1,47 @@
 <?php include_once('inc/header.php'); ?>
 <main>
-    <section></section>
-    <div>
+    <section class="background-section">
+        <img src="assets/images/background-image.jpeg" alt="">
+    </section>
+    <div class="main-consult">
         <ul>
-            <li><a href="#contact">Consultation individuelle au cabinet</a></li>
-            <li><a href="#contact">Consultation individuelle à domicile</a></li>
+            <li><i class="fa-solid fa-shop"></i><a href="#contact">Consultation individuelle au cabinet</a></li>
+            <li><i class="fa-solid fa-house"></i><a href="#contact">Consultation individuelle à domicile</a></li>
         </ul>
     </div>
-    <div>
+    <div class="main-bilan">
         <ul>
-            <li><a href="#contact">Bilan cognitif et stimulation cognitive</a></li>
-            <li><a href="#contact">Café des aidants</a></li>
+            <li><i class="fa-solid fa-brain"></i><a href="#contact">Bilan cognitif et stimulation cognitive</a></li>
+            <li id="main-bilan-li"><i class="fa-solid fa-mug-saucer"></i><a href="#contact">Café des aidants</a></li>
         </ul>
+    </div>
+    <div class="container-downbelow">
+        <div class="container-up">
+            <div class="container-up-img">
+                <img src="assets/images/salon.jpeg" alt="salon">
+            </div>
+            <div class="container-up-location">
+                <div>
+                    <i class="fa-solid fa-location-dot"></i>
+                </div>
+                <h3>
+                    3 chemin de Douville
+                    97180 Sainte- Anne
+                    GUADELOUPE
+                </h3>
+            </div>
+        </div>
+        <div class="container-middle">
+            <h3>Pourquoi consulter ou conseiller à un proche de consulter ? </h3>
+            <p>Il peut être difficile d'affronter des problèmes psychologiques et d'en discuter. La peur de
+                l'échec, de l'humiliation ou de la désapprobation peut vous retenir. Cependant, en ignorant ces
+                problèmes, ils peuvent s'aggraver et dominer votre vie. Consultez un psychologue à domicile si
+                vous préférez rester chez vous ou si vous manquez de temps pour des déplacements réguliers.
+                Cette option vous offre le confort de votre maison sans soucis de transport. Si vous avez un
+                handicap ou une perte d'autonomie, il est important de recourir à un psychologue à domicile
+                pour une organisation pratique de votre séance</p>
+        </div>
+        <div class="container-down"><button>Newsletter</button></div>
     </div>
 </main>
 <?php include_once('inc/footer.php'); ?>
