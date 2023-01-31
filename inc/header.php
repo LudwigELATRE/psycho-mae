25b912c9470b7fb922c10307c0c45de81bea3f2e
LudwigELATRE <73239553+LudwigELATRE@users.noreply.github.com>
2023-01-31 13:51:58 +0100

    set up from the beginning of the project
    set up from the beginning of the project

---
+++ inc/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Psychologue</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <div>
+            <img src="" alt="logo">
+        </div>
+        <div>
+            <div>
+                <h1>Psychologue clinicienne</h1>
+                <h4>Spécialisée en gérontologie, psychothérapie adultes et personnes âgées</h4>
+            </div>
+            <div>
+                <ul>
+                    <li><a href="#contact">A propos de moi</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="#contact">Therapie</a></li>
+                    <li><a href="#contact">Tarifs</a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php include_once('inc/header.php'); ?>
+<main>
+    <section></section>
+    <div>
+        <ul>
+            <li><a href="#contact">Consultation individuelle au cabinet</a></li>
+            <li><a href="#contact">Consultation individuelle à domicile</a></li>
+        </ul>
+    </div>
+    <div>
+        <ul>
+            <li><a href="#contact">Bilan cognitif et stimulation cognitive</a></li>
+            <li><a href="#contact">Café des aidants</a></li>
+        </ul>
+    </div>
+</main>
+<?php include_once('inc/footer.php'); ?>
